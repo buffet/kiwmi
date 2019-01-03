2631d607fc3812f16b3d7512ec1caccd67c4ec3d
buffet <niclas@countingsort.com>
2019-01-03 23:15:28 +0100

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use log::info;
+
+use wlroots::{
+    compositor,
+    utils::log::{init_logging, WLR_INFO},
+};
+
 fn main() {
-    wlroots::compositor::Builder::new().build_auto(()).run()
+    init_logs();
+
+    build_compositor().run();
+}
+
+fn init_logs() {
+    init_logging(WLR_INFO, None);
+
+    env_logger::Builder::from_env(
+        env_logger::Env::default()
+            .filter("KIWMI_LOG")
+    ).init();
+
+    info!("Logger initialized!");
+}
+
+fn build_compositor() -> compositor::Compositor {
+    compositor::Builder::new()
+        .gles2(true)
+        .data_device(true)
+        .build_auto(())
 }
